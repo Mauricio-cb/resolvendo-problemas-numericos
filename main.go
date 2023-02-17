24dfc05180cc5432a1e9e99e0fae5b4c31ec6101
Mauricio <mbispo1202@gmail.com>
2023-02-17 03:05:28 +0000

    Finalizando a segunda Parte

--- main.go
+++ main.go
@@ -3,19 +3,24 @@ package main
 import "fmt"
 
 func main()  {
-	fmt.Println(isMultipleOf(3, 100))
-	fmt.Println(isMultipleOf(5, 100))
+	fmt.Println(isMultipleOf(100))
+
 }
 
-func isMultipleOf(number int16, rangeOf int16) []int16{
 
-	 var multiples []int16;
+func isMultipleOf(rangeOf int16) map[int16] string{
 
+	multiples := make(map[int16] string);
+ 
 	for i := -1; i < int(rangeOf); rangeOf-- {
-		if rangeOf % number == 0 {
-			multiples = append(multiples, rangeOf)
+		if rangeOf % 3 == 0 {
+			multiples[rangeOf] = "Pin\n"
+		}else if rangeOf % 5 == 0 {
+			multiples[rangeOf] = "Pan\n"
+		}else{
+			multiples[rangeOf] = ""
 		}
 	}
 
-	return  multiples
-}
+	return multiples
+} 
